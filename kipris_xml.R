8eab6fea45e347b1685af4902661ee37d67b88f6
Jin0331 <sempre813@hallym.ac.kr>
2021-04-02 16:50:17 +0900

    update

--- kipris_xml.R
+++ kipris_xml.R
@@ -3,6 +3,8 @@ library(httr)
 library(tidyverse)
 library(glue)
 
+# general search info page : https://plus.kipris.or.kr/portal/data/service/DBII_000000000000001/view.do?menuNo=200100&kppBCode=&kppMCode=&kppSCode=&subTab=SC001&entYn=N&clasKeyword=#soap_ADI_0000000000002130
+
 auth_key <- "1fCRg1mSN=fYyE8OQdfhC6MZxEnDsjDmPsSbsPi0T=s="
 url <- "http://plus.kipris.or.kr/kipo-api/kipi/patUtiModInfoSearchSevice/"
 
@@ -11,12 +13,20 @@ search_keyword <- "TP53"
 numRows <- 200
 
 # General Search extraction
-general_search <- paste0(url, "getWordSearch?word=",search_keyword,"&year=0","&numOfRows=",numRows,"&ServiceKey=",auth_key)
-all_xml_node <- httr::GET(general_search) %>% httr::content(encoding = "UTF-8")
+general_search <- paste0(url, "getWordSearch?word=",search_keyword,"&year=0&ServiceKey=",auth_key)
+all_xml_node <- httr::GET(general_search) %>% httr::content(encoding = "UTF-8") # raw node
 
 total_count <- all_xml_node %>% xml_find_all(".//totalCount") %>% xml_text() %>% as.integer()
 total_page <- round(total_count / numRows) + 1
+
+# each page search
+page_search <- paste0(url, "getWordSearch?word=",search_keyword,"&year=0","&numOfRows=",numRows,"","&ServiceKey=",auth_key)
+
 register_number <- all_xml_node %>% xml_find_all(".//body") %>% xml_find_first(".//applicationNumber") %>% xml_text()
+
+
+
+
 
 # Bibliography extraction
 
