c7bd719e5a6d70630236a1f42289156c1d0c2136
Jin0331 <sempre813@hallym.ac.kr>
2021-04-02 16:44:11 +0900

    update

--- kipris_xml.R
+++ kipris_xml.R
@@ -5,15 +5,17 @@ library(glue)
 
 auth_key <- "1fCRg1mSN=fYyE8OQdfhC6MZxEnDsjDmPsSbsPi0T=s="
 url <- "http://plus.kipris.or.kr/kipo-api/kipi/patUtiModInfoSearchSevice/"
-search_keyword <- "TP53"
-numRows <- "10"
 
-# registerNumber extraction
-general_search <- paste0(url, "getWordSearch?word=",search_keyword,"&year=0&ServiceKey=",auth_key)
+# General Search keyword
+search_keyword <- "TP53"
+numRows <- 200
+
+# General Search extraction
+general_search <- paste0(url, "getWordSearch?word=",search_keyword,"&year=0","&numOfRows=",numRows,"&ServiceKey=",auth_key)
 all_xml_node <- httr::GET(general_search) %>% httr::content(encoding = "UTF-8")
 
 total_count <- all_xml_node %>% xml_find_all(".//totalCount") %>% xml_text() %>% as.integer()
-
+total_page <- round(total_count / numRows) + 1
 register_number <- all_xml_node %>% xml_find_all(".//body") %>% xml_find_first(".//applicationNumber") %>% xml_text()
 
 # Bibliography extraction
