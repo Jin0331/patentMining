ade490ccc6fcdb712f37e32d1aa417a46c2f9e4b
Jin0331 <sempre813@hallym.ac.kr>
2021-04-05 07:17:55 +0900

    update

--- kipris_xml.R
+++ kipris_xml.R
@@ -14,21 +14,18 @@ numRows <- 100
 
 # General Search extraction
 general_search <- paste0(url, "getWordSearch?word=",search_keyword,"&year=0&ServiceKey=",auth_key)
-all_xml_node <- httr::GET(general_search) %>% httr::content(encoding = "UTF-8") # raw node
+xml_node <- httr::GET(general_search) %>% httr::content(encoding = "UTF-8") # raw node
 
 total_count <- all_xml_node %>% xml_find_all(".//totalCount") %>% xml_text() %>% as.integer()
 total_page <- round(total_count / numRows) + 1
 
 # each page search
-while,, function
-page_search <- paste0(url, "getWordSearch?word=",search_keyword,"&year=0", "&pageNo=", page,"&numOfRows=",numRows,"","&ServiceKey=",auth_key)
-register_number <- all_xml_node %>% xml_find_all(".//body") %>% xml_find_first(".//applicationNumber") %>% xml_text()
-
-
-
+# page 1 시작
+page_search <- paste0(url, "getWordSearch?word=",search_keyword,"&year=0", "&pageNo=", page, "&numOfRows=",numRows,"","&ServiceKey=", auth_key)
+all_xml_node <- httr::GET(page_search) %>% httr::content(encoding = "UTF-8")
+register_number <- all_xml_node %>% xml_find_all(".//body") %>% xml_find_all(".//applicationNumber") %>% xml_text()
 
 
 # Bibliography extraction
-
 biblio_search <- paste0(url, "getBibliographyDetailInfoSearch?applicationNumber=",register_number,"&ServiceKey=", auth_key)
 
